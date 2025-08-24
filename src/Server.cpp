bdb390a435b491d93ee27a44b03bdd5d86a72487
Jansen Ken Pegrasio <kenpegrasio@gmail.com>
2025-08-24 16:54:52 +0800

    feat: echo command

--- src/Server.cpp
+++ src/Server.cpp
@@ -4,21 +4,113 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <algorithm>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
 #include <string>
 #include <thread>
+#include <vector>
 
-void process_request(int client_fd) {
-  char buffer[1024];
+bool is_number(char x) { return x >= '0' && x <= '9'; }
+
+unsigned long long read_size(std::string &buffer, int &ptr) {
+  unsigned long long ans = 0;
+  while (is_number(buffer[ptr])) {
+    ans = ans * 10 + int(buffer[ptr] - '0');
+    ptr++;
+  }
+  return ans;
+}
+
+std::string read_string(std::string &buffer, int &ptr) {
+  ptr++;
+  auto str_len = read_size(buffer, ptr);
+  ptr += 2;
+  std::string res = "";
+  for (int i = 0; i < str_len; i++) {
+    res += buffer[ptr];
+    ptr++;
+  }
+  return res;
+}
+
+std::vector<std::string> read_array(std::string &buffer, int &ptr) {
+  ptr++;
+  int number_of_element = read_size(buffer, ptr);
+  ptr += 2;
+  std::vector<std::string> res;
+  for (int i = 0; i < number_of_element; i++) {
+    auto str = read_string(buffer, ptr);
+    ptr += 2;
+    res.push_back(str);
+  }
+  return res;
+}
+
+const int BUFFER_SIZE = 4096;
+
+std::string read_request(int client_socket) {
+  std::string res = "";
+  char buffer[BUFFER_SIZE] = {0};
+  int bytes_read = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
+  if (bytes_read == 0) {
+    std::cerr << "Empty request" << std::endl;
+    close(client_socket);
+    return "";
+  }
+  if (bytes_read <= 0) {
+    std::cerr << "Failed to receive data from client" << std::endl;
+    close(client_socket);
+    return "";
+  }
+  buffer[bytes_read] = '\0';
+  res += buffer;
+  return res;
+}
+
+std::string send_string_response(std::string str) {
+  std::string response = "$";
+  int sz = str.size();
+  std::vector<int> digits;
+  while (sz > 0) {
+    digits.push_back(sz % 10);
+    sz = sz / 10;
+  }
+  reverse(digits.begin(), digits.end());
+  for (auto digit : digits) {
+    response.push_back(digit + '0');
+  }
+  response += "\r\n";
+  response += str;
+  response += "\r\n";
+  return response;
+}
+
+void process_client(int client_socket) {
   while (true) {
-    memset(buffer, 0, sizeof(buffer));
-    int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
-    if (bytes_received <= 0) break;
+    std::string res = read_request(client_socket);
+    if (res == "") return;
 
-    std::string response = "+PONG\r\n";
-    send(client_fd, response.c_str(), response.size(), 0);
+    int ptr = 0;
+    auto elements = read_array(res, ptr);
+
+    // std::cout << "Buffer: " << std::endl;
+    // for (auto c : std::string(buffer)) {
+    //   std::cout << std::hex << static_cast<int>(c) << " ";
+    // }
+    // std::cout << std::endl;
+
+    if (elements[0] == "PING") {
+      std::string response = "+PONG\r\n";
+      send(client_socket, response.c_str(), response.size(), 0);
+    } else if (elements[0] == "ECHO") {
+      std::string response = send_string_response(elements[1]);
+      send(client_socket, response.c_str(), response.size(), 0);
+    } else {
+      std::string response = "+PONG\r\n";
+      send(client_socket, response.c_str(), response.size(), 0);
+    }
   }
 }
 
@@ -72,7 +164,7 @@ int main(int argc, char **argv) {
     int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
                            (socklen_t *)&client_addr_len);
     std::cout << "Client connected\n";
-    std::thread t(process_request, client_fd);
+    std::thread t(process_client, client_fd);
     t.detach();
   }
   close(server_fd);
